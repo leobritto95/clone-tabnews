16feb43a67d6979ca569f62b73456287a0b1029f
leobritto95 <46280859+leobritto95@users.noreply.github.com>
2023-11-28 06:17:51 +0000

    modifica frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Nova Frase</h1>
+    return <h1>Obrigado pela oportunidade que me deu a quase 5 anos atrás, hoje somos socios e conquistaremos o mundo 😎.</h1>
 }
 
 export default Home;
