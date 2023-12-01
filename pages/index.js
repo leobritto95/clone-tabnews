342e49f8490e88ec285f5ab79cb32389f8aafa29
leobritto95 <46280859+leobritto95@users.noreply.github.com>
2023-12-01 04:32:50 +0000

    adiciona scripts `list:check` e `list:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Obrigado pela oportunidade que me deu a quase 5 anos atrás, hoje somos socios e conquistaremos o mundo 😎.</h1>
+function Home() {
+  return (
+    <h1>
+      Obrigado pela oportunidade que me deu a quase 5 anos atrás, hoje somos
+      socios e conquistaremos o mundo 😎.
+    </h1>
+  );
 }
 
 export default Home;
