d6871d0ca5330a5d044a5f94b3e68caaefe73269
leobritto95 <46280859+leobritto95@users.noreply.github.com>
2023-12-18 07:28:31 +0000

    move `.env`file to `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,6 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  console.log(database);
   const result = await database.query("SELECT 1+1 as sum;");
   console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
